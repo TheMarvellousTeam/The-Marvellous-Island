e6dcd98d63ef742d5792f652b1298bd6856b0beb
NiZiL <biasutto.t@gmail.com>
2015-01-23 23:49:15 +0100

    threaded server, go P4ddl3

--- server/server.py
+++ server/server.py
@@ -1,6 +1,22 @@
 #!/usr/bin/python3
 
 import socket
+import threading
+
+class RemoteListener(threading.Thread):
+    def __init__(self, connection):
+        threading.Thread.__init__(self)
+        self.connection = connection
+
+    def listen(self):
+        while True:
+            data = self.connection.recv(16)
+            if data:
+                print(data)
+
+    def close(self):
+        self.connection.close()
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -11,12 +27,10 @@ sock.listen(1)
 while True:
     connection, client_addr = sock.accept()
     print(client_addr)
+    listener = RemoteListener(connection)
     try:
-        while True:
-            data = connection.recv(16)
-            if data:
-                print(data)
+        listener.listen()
     finally:
-        connection.close()
+        listener.close()
 
 sock.close()        
