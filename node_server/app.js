e16e7e2e7c5b2dc9b59e321e895b72603691b536
NiZiL <biasutto.t@gmail.com>
2015-01-24 18:21:28 +0100

    node_server, remote for socket ok

--- node_server/app.js
+++ node_server/app.js
@@ -2,6 +2,10 @@ var app = require('express')()
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
 var game = require('./game')
+var net = require('net')
+
+var port = 31415
+var ip = '192.168.1.1'
 
 var rooms = [{
     viewers : [],
@@ -9,6 +13,19 @@ var rooms = [{
     game : game
 }]
 
+var remoteServer = net.createServer( function(sock) {
+
+    console.log('['+sock.remoteAddress+'] connected')
+
+    sock.on('end', function(){
+    	console.log('['+sock.remoteAddress+'] deconnected')
+    })
+	
+    sock.on('data', function(data){
+    	console.log('['+sock.remoteAddress+'] '+data)
+    })
+   
+})
 
 
 io.sockets.on('connection', function (socket) {
@@ -20,6 +37,9 @@ io.sockets.on('connection', function (socket) {
 })
 
 
+remoteServer.listen(port, ip, function(){
+	console.log('server bound')
+})
 
 
 server.listen( 1984 )
