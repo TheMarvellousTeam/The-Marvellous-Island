3f2810d38dabe841b4564c07a208c5cd2544eed5
NiZiL <biasutto.t@gmail.com>
2015-01-24 14:53:18 +0100

    split server.py

--- server/server.py
+++ server/server.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 
-import asyncore
-import socket
 import json
-
-def out(s):
-  return s.encode("utf-8")
+import network
 
 
 class World:
@@ -76,89 +72,15 @@ class World:
     self.render.send_msg(json.dumps(response))
     self.broadcast("NEW_TURN")
 
-class RemoteHandler(asyncore.dispatcher_with_send):
-
-  def __init__(self, sock, addr, world):
-    asyncore.dispatcher_with_send.__init__(self, sock)
-    self.addr = addr
-    self.world = world
-
-  def handle_read(self):
-    data = self.recv(512)
-    if data:
-      self.world.receive_cmd(self.addr, data)
-
-  def handle_close(self):
-    self.close()
-
-  def send_msg(self, msg):
-    self.send(out(msg))
-
-
-class RenderHandler(asyncore.dispatcher_with_send):
-
-  def __init__(self, sock, world):
-    asyncore.dispatcher_with_send.__init__(self, sock)
-    self.world = world
-
-  def handle_read(self):
-    data = self.recv(512)
-    if data:
-      self.world.receive_msg(data)
-
-  def send_msg(self, msg):
-    self.send(out(msg))
-
-  def handle_close(self):
-    self.close()
-
-
-class RemoteServer(asyncore.dispatcher):
-
-  def __init__(self, host, port, world):
-    asyncore.dispatcher.__init__(self)
-    self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.set_reuse_addr()
-    self.bind((host, port))
-    self.listen(world.nb_players)
-
-    self.world = world
-
-  def handle_accept(self):
-    pair = self.accept()
-    if pair is not None:
-      sock, addr = pair
-      self.world.add_player(addr, RemoteHandler(sock, addr, world))
-
-  def handle_close(self):
-    self.close()
-
-
-class RenderServer(asyncore.dispatcher):
-  
-  def __init__(self, host, port, world):
-    asyncore.dispatcher.__init__(self)
-    self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
-    self.set_reuse_addr()
-    self.bind((host, port))
-    self.listen(1)
-
-    self.world = world
-
-  def handle_accept(self):
-    pair = self.accept()
-    if pair is not None:
-      sock, addr = pair
-      self.world.init_render(RenderHandler(sock, self.world))
 
 
 
 
 def main(ip, portRemote, portRender):
   world = World(1)
-  remote = RemoteServer(ip, portRemote, world)
-  render = RenderServer(ip, portRender, world)
-  asyncore.loop()
+  remote = network.RemoteServer(ip, portRemote, world)
+  render = network.RenderServer(ip, portRender, world)
+  network.start()
 
 
 if __name__=="__main__":
