90cd7ce90e66e31202a30cedb316c4e5957a4e34
Platane <arthur@arthur-brongniart.fr>
2015-01-28 17:04:33 +0100

    spec in jasmin 1.x

--- node_server_test/specs/mechanics/end-to-end.js
+++ node_server_test/specs/mechanics/end-to-end.js
@@ -5,6 +5,20 @@ var Game = require( '../../../node_server/system/game')
 var GameLoop = require( '../../../node_server/handler/gameLoop')
 var Ed = require( '../../../node_server/util/eventDispatcher')
 
+var callBack = function(){
+
+    var k= 0
+    var fn = function(){
+        k++
+    }
+
+    fn.count = function(){
+        return k
+    }
+
+    return fn
+}
+
 describe('', function(){
     beforeEach(function(){
         this.ed = Object.create( Ed )
@@ -14,11 +28,15 @@ describe('', function(){
             ed: this.ed
         })
     })
+    afterEach(function(){
+        this.ed.reset()
+    })
     describe('gameLoop ', function(){
 
         // wait
-        beforeEach(function( done ){
-            setTimeout( done, 500)
+        beforeEach(function( ){
+            //setTimeout( done, 500)
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000
         })
 
         // build simple world
@@ -36,7 +54,8 @@ describe('', function(){
                     obstacle : false
                 })
             }
-            this.game.world = w
+            this.game.world.length
+            this.game.world.push.apply( this.game.world, w )
 
             // force world update
             this.ed.dispatch('io:viewer-connect')
@@ -57,13 +76,13 @@ describe('', function(){
                 this.ed.dispatch('io:viewer-connect')
             })
 
-            describe('start / end resolution', function(){
+            describe('start / end resolution with basic commands', function(){
 
                 beforeEach(function( done ){
 
                     // listen
-                    this.ed.listen('gameLoop:start-resolution', (this.cb = jasmine.createSpy('cb')) )
-                    this.ed.listen('gameLoop:end-resolution', function(){ done() })
+                    this.ed.listen('gameLoop:start-resolution', this.cb = callBack() )
+                    this.ed.listen('gameLoop:end-resolution', done )
 
                     this.ed.dispatch('io:player-cmds', {
                         id: 'ana',
@@ -72,8 +91,64 @@ describe('', function(){
                 })
 
                 it('start / end resolution called', function(){
-                    expect( this.cb.calls.count() ).toBe( 1 )
+                    expect( this.cb.count() ).toBe( 1 )
                 })
+            })
+
+            describe('start / end resolution with basic commands 2', function(){
+
+                beforeEach(function( done ){
+
+                    // listen
+                    this.ed.listen('gameLoop:start-resolution', this.cb = callBack() )
+                    this.ed.listen('gameLoop:end-resolution', done )
+
+                    this.ed.dispatch('io:player-cmds', {
+                        id: 'ana',
+                        cmds: [
+                            {'action': 'move', 'directionX':  1, 'directionY': 0 },
+                            {'action': 'move', 'directionX': -1, 'directionY': 0 },
+                            {'action': 'move', 'directionX': -1, 'directionY': 0 },
+                            {'action': 'move', 'directionX':  1, 'directionY': 0 }
+                        ]
+                    })
+                })
+
+                it('start / end resolution called', function(){
+                    expect( this.cb.count() ).toBe( 1 )
+                })
+            })
+
+            describe('start / end resolution when dead', function(){
+
+                beforeEach(function( done ){
+
+                    var k = 1
+
+                    // listen
+                    this.ed.listen('gameLoop:start-resolution', this.cbS = callBack() )
+                    this.ed.listen('gameLoop:end-resolution', this.cbE = callBack() )
+                    //this.ed.listen('gameLoop:end-resolution', function(){ if( !(k--) ) done() })
+
+                    // kill it
+                    this.ed.dispatch('io:player-cmds', {
+                        id: 'ana',
+                        cmds: [
+                            {'action': 'peck'},
+                            {'action': 'peck'},
+                            {'action': 'move', 'directionX': 1, 'directionY': 0 },
+                            {'action': 'move', 'directionX': 1, 'directionY': 0 }
+                        ]
+                    })
+                    
+                    setTimeout( done, 9500 )
+                })
+
+                it('start / end resolution should be called multiple times', function(){
+                    expect( this.cbS.count() > 1 ).toBe( true )
+                    expect( this.cbE.count() > 1 ).toBe( true )
+                })
+
             })
 
         })
